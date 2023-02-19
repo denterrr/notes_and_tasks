0886dcd0bb3970dd45e12f6616c2957c185e4807
Denis <denter100@mail.ru>
2023-02-19 23:43:39 +0300

    init commit

---
+++ app/src/main/java/ter/den/notesandtasks/MyReceiver.kt
@@ -0,0 +1,27 @@
+package ter.den.notesandtasks
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.setPadding
+
+class MyReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val intentt = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intentt, PendingIntent.FLAG_IMMUTABLE)
+        val notificationManager = context?.getSystemService(NotificationManager::class.java)
+        intent?.getIntExtra("id", -1)
+        val builder = Notification.Builder(context, "CHANNEL_ID")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("IS TITLE")
+            .setContentText("1")
+            .setContentIntent(pendingIntent)
+        notificationManager?.notify(1, builder.build())
+    }
+}
---
+++ app/src/main/java/ter/den/notesandtasks/MyService.kt
@@ -0,0 +1,40 @@
+package ter.den.notesandtasks
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.util.Log
+import android.widget.Toast
+import androidx.work.*
+import java.util.concurrent.TimeUnit
+
+
+class MyService(context: Context, workerParameters: WorkerParameters) : Worker(context,workerParameters){
+    override fun doWork(): Result {
+
+        val count = inputData.getInt("int", 0)
+
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val notificationManager = applicationContext.getSystemService(NotificationManager::class.java)
+
+        val builder = Notification.Builder(applicationContext, "CHANNEL_ID")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("IS TITLE")
+            .setContentText(count.toString())
+            .setContentIntent(pendingIntent)
+        notificationManager.notify(count, builder.build())
+
+        val work = OneTimeWorkRequestBuilder<MyService>()
+            .setInitialDelay(15, TimeUnit.SECONDS)
+            .setInputData(Data.Builder().putInt("int", count+1).build())
+            .build()
+        val manager = WorkManager.getInstance(applicationContext)
+        manager.beginUniqueWork("work$count", ExistingWorkPolicy.KEEP, work).enqueue()
+
+        return Result.success()
+    }
+}
